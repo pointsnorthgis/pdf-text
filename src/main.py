282e4156e7f4f9a2573787a870d9be76864773b8
pointsnorthgis <pointsnorthgis@gmail.com>
2020-10-17 12:03:23 -0700

    create parser for pdf with text

--- src/main.py
+++ src/main.py
@@ -1,1 +1,32 @@
+'''
+    Program for reading through PDF documents and returning
+    a list of words.
+'''
 import os
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+
+class PdfText(object):
+    def __init__(self, pdf_path, password=None):
+        self.pdf_path = pdf_path
+        self.pdf_password = password
+
+    def parse_pdf(self):
+        output_string = StringIO()
+        with open(self.pdf_path, 'rb') as in_file:
+            parser = PDFParser(in_file)
+            doc = PDFDocument(parser)
+            rsrcmgr = PDFResourceManager()
+            device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            for page in PDFPage.create_pages(doc):
+                interpreter.process_page(page)
+                
+            print(output_string.getvalue())
