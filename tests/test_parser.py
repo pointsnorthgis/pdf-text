13da09721b8b765763d1b4f2f1ebf8cb3eff9451
pointsnorthgis <pointsnorthgis@gmail.com>
2020-10-17 12:58:36 -0700

    update tests with pdf words and list

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
 import unittest
+import io
 from src.main import PdfText
 
 class TestParser(unittest.TestCase):
@@ -9,8 +10,13 @@ class TestParser(unittest.TestCase):
     def test_pdf_text(self):
         pdf_path = r'C:\dev\pdf-text\test_data\howto-argparse.pdf'
         self.pdf_parser.pdf_path = pdf_path
-        result = self.pdf_parser.parse_pdf()
-        self.assertEqual(result, True)
+        self.assertIsInstance(
+            self.pdf_parser.parse_pdf(), io.StringIO
+            )
+        self.pdf_parser.get_pdf_string()
+        self.assertIsInstance(self.pdf_parser.pdf_string, str)
+        self.pdf_parser.get_pdf_words()
+        self.assertIsInstance(self.pdf_parser.pdf_words, list)
 
 
 if __name__ == '__main__':
