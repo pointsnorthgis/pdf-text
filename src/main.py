7e59206ec670286e79c9eb27b20dba16acaac6b3
pointsnorthgis <pointsnorthgis@gmail.com>
2020-10-19 17:06:18 -0700

    make pdf string and list more verbose

--- src/main.py
+++ src/main.py
@@ -17,13 +17,19 @@ class PdfText(object):
     def __init__(self, pdf_path=None, password=None):
         self.pdf_path = pdf_path
         self.pdf_password = password
+        self.pdf_string = None
+        self.pdf_words = []
 
     def get_pdf_string(self):
         string_io = self.parse_pdf()
         self.pdf_string = string_io.getvalue()
+        return self.pdf_string
 
     def get_pdf_words(self):
+        if self.pdf_string is None:
+            self.get_pdf_string()
         self.pdf_words = self.pdf_string.split()
+        return self.pdf_words
 
     def parse_pdf(self):
         output_string = StringIO()
