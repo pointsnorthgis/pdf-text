4445ed20c1ca534af8d2b083566f53c69352b8b0
zack-king <kingurr@gmail.com>
2020-10-19 21:54:01 -0700

    add place holder for OCR function

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@
 import os
 from io import StringIO
 
+from PIL import Image
+import pytesseract
+
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfdocument import PDFDocument
@@ -18,12 +21,19 @@ class PdfText(object):
         self.pdf_path = pdf_path
         self.pdf_password = password
         self.pdf_string = None
-        self.pdf_words = []
+        self.pdf_words = None
+        self.pdf_is_image = False
 
     def get_pdf_string(self):
         string_io = self.parse_pdf()
-        self.pdf_string = string_io.getvalue()
-        return self.pdf_string
+        pdf_string = string_io.getvalue()
+        if len(pdf_string) > 0:
+            self.pdf_string = pdf_string
+            return self.pdf_string
+
+        # see if pdf is image with text
+        else:
+            return None
 
     def get_pdf_words(self):
         if self.pdf_string is None:
@@ -32,14 +42,26 @@ class PdfText(object):
         return self.pdf_words
 
     def parse_pdf(self):
+        '''
+        Open a PDF document and extract text from all pages
+        '''
         output_string = StringIO()
         with open(self.pdf_path, 'rb') as in_file:
             parser = PDFParser(in_file)
             doc = PDFDocument(parser)
             rsrcmgr = PDFResourceManager()
-            device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+            device = TextConverter(
+                rsrcmgr, output_string, laparams=LAParams()
+                )
             interpreter = PDFPageInterpreter(rsrcmgr, device)
             for page in PDFPage.create_pages(doc):
                 interpreter.process_page(page)
                 
             return output_string
+
+    def parse_pdf_image(self):
+        '''
+        Open a PDF that contains text embedded in 
+        images and extract text using OCR
+        '''
+        return
